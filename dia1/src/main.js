25707ac7b652da25df232afbd1d8d5a8df81c9d4
Cristiano Silva <crjstjanojose@gmail.com>
2021-08-17 12:48:57 -0300

    Toggle Div App

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -4,3 +4,13 @@ document.querySelector('[data-js="app"]').innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `;
+
+const btn = document.querySelector('[data-js="toggle"]');
+
+btn.addEventListener("click", (e) => {
+  const divApp = document.querySelector('[data-js="app"]');
+
+  divApp.hasAttribute("hidden")
+    ? divApp.removeAttribute("hidden")
+    : divApp.setAttribute("hidden", true);
+});
